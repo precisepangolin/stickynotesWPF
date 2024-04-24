bef4cf79fb92676a0b2e4c070ea8940ff45ccf99
Milena Kolakowska <nekoloid@gmail.com>
2024-04-24 23:16:01 +0200

    gitignore

--- StickyNotesWPF/obj/Debug/net6.0-windows/MainWindow.g.i.cs
+++ StickyNotesWPF/obj/Debug/net6.0-windows/MainWindow.g.i.cs
@@ -53,7 +53,7 @@ namespace StickyNotesWPF {
                 return;
             }
             _contentLoaded = true;
-            System.Uri resourceLocater = new System.Uri("/StickyNotesWPF;component/mainwindow.xaml", System.UriKind.Relative);
+            System.Uri resourceLocater = new System.Uri("/StickyNotesWPF;V1.0.0.0;component/mainwindow.xaml", System.UriKind.Relative);
             
             #line 1 "..\..\..\MainWindow.xaml"
             System.Windows.Application.LoadComponent(this, resourceLocater);
