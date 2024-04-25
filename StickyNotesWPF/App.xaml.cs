7b59d80e9bd693207e442d0484b1b64273e3cf4b
Milena Kolakowska <nekoloid@gmail.com>
2024-04-25 20:48:55 +0200

    Added textbox with semi-transparent bg over bg elements

--- StickyNotesWPF/App.xaml.cs
+++ StickyNotesWPF/App.xaml.cs
@@ -13,5 +13,11 @@ namespace StickyNotesWPF
     /// </summary>
     public partial class App : Application
     {
+        private void Application_Startup(object sender, StartupEventArgs e)
+        {
+            MainWindow wnd = new MainWindow();
+            wnd.Title = "sTICKY nOTES";
+            wnd.Show();
+        }
     }
 }
