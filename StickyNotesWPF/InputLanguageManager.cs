67d3754cb019fc4f0897f05d15c33d485400e4fc
Milena Kolakowska <nekoloid@gmail.com>
2024-05-02 21:47:21 +0200

    Fixed spellcheck for richtextbox

---
+++ StickyNotesWPF/InputLanguageManager.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StickyNotesWPF
+{
+    public sealed class InputLanguageManager : System.Windows.Threading.DispatcherObject
+    {
+    }
+}
